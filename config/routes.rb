34e480c47a0a51e2c4bb94daaea8c04de4b46fd2
kwngo <kareem.kwong@gmail.com>
2015-03-12 17:46:29 +0800

    Added api endpoints for current_user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_scope :user do
+    get '/api/current_user' => 'users/sessions#show_current_user'
+    post '/api/check/is_user' => 'users/users#is_user', as: 'is_user'
+  end
+  namespace :api do
 
+  end
   root 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
