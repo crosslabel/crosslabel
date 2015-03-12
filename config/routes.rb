03b4543dc47d2bcf74a9e633e9ab3af4c91fa3c5
kwngo <kareem.kwong@gmail.com>
2015-03-12 19:43:19 +0800

    Finished basic user controller specs

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,11 @@ require 'api_constraints'
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   devise_scope :user do
-    get '/api/current_user' => 'users/sessions#show_current_user'
-    post '/api/check/is_user' => 'users/users#is_user', as: 'is_user'
   end
 
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api'}, path: '/' do
     scope :module => :v1, constraints: ApiConstraints.new(version: 1, default: true) do
-      resources :users, :only => [:show]
+      resources :users, :only => [:show, :create, :update]
     end
   end
   root 'home#index'
--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::UsersController, type: :controller do
-    before(:each) { request.headers['Accept'] = "application/vnd.marketplace.v1"}
+    before(:each) { request.headers['Accept'] = "application/vnd.brandly.v1"}
 
     describe "GET #show" do
       before(:each) do
@@ -17,5 +17,70 @@ RSpec.describe Api::V1::UsersController, type: :controller do
       it { should respond_with 200}
     end
 
+    describe "POST #create" do
+      context "when valid attributes" do
+        before(:each) do
+          @user_attributes = FactoryGirl.attributes_for :user
+          post :create, { user: @user_attributes}, format: :json
+        end
 
+        it "renders the json representation for the user record just created" do
+          user_response = JSON.parse(response.body, symbolize_names: true)
+          expect(user_response[:email]).to eql @user_attributes[:email]
+        end
+
+        it { should respond_with 201 }
+      end
+
+      context "with invalid attributes" do
+        before(:each) do
+          @invalid_user_attributes = { password: '12345678', password_confirmation: '87654321'}
+          post :create, { user: @invalid_user_attributes }, format: :json
+        end
+
+        it "renders an errors json" do
+          user_response = JSON.parse(response.body, symbolize_names: true)
+          expect(user_response).to have_key(:errors)
+        end
+
+        it "renders the json errors on why the user could not be created" do
+          user_response = JSON.parse(response.body, symbolize_names: true)
+          expect(user_response[:errors][:email]).to include "can't be blank"
+        end
+
+        it { should respond_with 422}
+      end
+    end
+
+    describe "PUT/PATCH #update" do
+      context "with valid attributes" do
+        before(:each) do
+          @user = FactoryGirl.create :user
+          patch :update, { id: @user.id, user: { email: 'newmail@example.com'}}, format: :json
+        end
+
+        it "renders the json representation for the updated user" do
+          user_response = JSON.parse(response.body, symbolize_names: true)
+          expect(user_response[:email]).to eql "newmail@example.com"
+        end
+        it { should respond_with 200}
+      end
+      context "with invalid attributes" do
+        before(:each) do
+          @user = FactoryGirl.create :user
+          patch :update, { id: @user.id, user: { email: "bademail.com" } }, format: :json
+        end
+
+        it "renders an errors json" do
+          user_response = JSON.parse(response.body, symbolize_names: true)
+          expect(user_response).to have_key(:errors)
+        end
+
+        it "renders the json errors on why the user could not be updated" do
+          user_response = JSON.parse(response.body, symbolize_names: true)
+          expect(user_response[:errors][:email]).to include "is invalid"
+        end
+        it { should respond_with 422 }
+      end
+    end
 end
