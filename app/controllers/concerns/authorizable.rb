99c1035dba499fc3ad0fcf775f9e8ad73c20b1e0
kwngo <kareem.kwong@gmail.com>
2015-03-14 21:33:32 +0800

    Removed request and response stubbing methods

--- app/controllers/concerns/authorizable.rb
+++ app/controllers/concerns/authorizable.rb
@@ -2,15 +2,7 @@ module Authorizable
   def current_user
     @current_user ||= User.find_by(auth_token: request.headers['Authorization'])
   end
-
-  # def request
-  #   request
-  # end
-  #
-  # def response
-  #   response
-  # end
-
+  
   def authenticate_with_token!
     render json: { errors: "Not authorized" }, status: :unauthorized unless current_user.present?
   end
