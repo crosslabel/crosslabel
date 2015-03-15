0c229c34c933ab217e996d0c240cf81cfedc09e7
kwngo <kareem.kwong@gmail.com>
2015-03-16 04:00:21 +0800

    Fixed routes rebase problem

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ require 'api_constraints'
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :products
-  resources :users, only: [:show]
+  resources :users do
+  end
 
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api'}, path: '/' do
     scope :module => :v1, constraints: ApiConstraints.new(version: 1, default: true) do
