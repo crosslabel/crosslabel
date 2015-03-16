705631970430036ed219719e63b6da76c9548d92
kwngo <kareem.kwong@gmail.com>
2015-03-17 05:49:44 +0800

    Added method to convert price to integer

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,10 @@
 class Product < ActiveRecord::Base
   validates_presence_of :unit_price, :title, :image, :link
+  before_save :convert_unit_price_to_int
+
+  private
+  
+  def convert_unit_price_to_int
+    self.unit_price = self.unit_price.gsub(/[^0-9]/,'').to_i
+  end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
+
+  before { @product = FactoryGirl.build(:product)}
+
+  subject { @product }
   it { should respond_to(:title)}
   it { should respond_to(:unit_price)}
   it { should respond_to(:image)}
@@ -10,4 +14,12 @@ RSpec.describe Product, type: :model do
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:unit_price) }
 
+  it { should be_valid }
+
+  describe "before save" do
+    it "should transform the unit price string into integer" do
+      @product.save
+      expect(@product.unit_price).to eq(36000)
+    end
+  end
 end
