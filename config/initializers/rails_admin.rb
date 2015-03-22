3b070b5f74759794c47e14a0578d15216a8f13db
kwngo <kareem.kwong@gmail.com>
2015-03-22 08:17:52 +0800

    Removed CanCan temporarily

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -7,11 +7,11 @@ RailsAdmin.config do |config|
   config.authenticate_with do
     warden.authenticate! scope: :user
   end
-  
+
   config.current_user_method(&:current_user)
 
   ## == Cancan ==
-  config.authorize_with :cancan
+  # config.authorize_with :cancan
 
   ## == PaperTrail ==
   config.audit_with :paper_trail, 'User', 'PaperTrail::Version' # PaperTrail >= 3.0.0
