ddb1582b30b8823f90566bf09be9f3ed330db701
kwngo <kareem.kwong@gmail.com>
2015-03-29 21:52:37 +0800

    Default action for visitors who vote

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
   include Authorizable
-
+  
   def logged_in_user
     unless current_user
       flash[:notice] = "Please log in."
