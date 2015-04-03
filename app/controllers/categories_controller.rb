c8e5f647c0942d979e92bb0f533b4d55b9997334
kwngo <kareem.kwong@gmail.com>
2015-04-03 18:48:42 +0800

    Built basic categories view

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,8 @@
 class CategoriesController < ApplicationController
   def index
+    @categories = Category.all
   end
-
   def show
+    @category = Category.find_by(title: params[:title])
   end
 end
