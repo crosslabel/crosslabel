49e2f38b527c85e5e0a64ba05dcdbecd82b18e4a
kwngo <kareem.kwong@gmail.com>
2015-04-04 20:08:08 +0800

    Added basic slide panel functionality for products

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ActiveRecord::Base
   searchkick autocomplete: ['title']
   has_many :upvotes, as: :upvotable
   has_and_belongs_to_many :categories
+  belongs_to :shop
   validates_presence_of :unit_price, :title, :image, :link
   before_save { self.unit_price = self.unit_price.gsub(/[^0-9]/,'').to_i }
   scope :mens, lambda { where(for_men: true)}
