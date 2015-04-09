adf4d8f2ff96bf9b136beca54c8cc583fe33db5d
kwngo <kareem.kwong@gmail.com>
2015-04-09 08:13:55 +0800

    Added recommends method

--- lib/recommendations/rater.rb
+++ lib/recommendations/rater.rb
@@ -4,6 +4,19 @@ require 'recommendations/rater/recommender'
 
 module Recommendations
   module Rater
+    extend ActiveSupport::Concern
 
+    module ClassMethods
+      def recommends(*things)
+        Recommendations.configure do |config|
+          config.ratable_classes = []
+          config.user_class = self
+        end
+
+        things.each { |thing| thing.to_s.classify.constantize.make_recommendable! }
+
+        
+      end
+    end
   end
 end
