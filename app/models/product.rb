e6758415dedf70d59f75067761aacd96d86765ef
kwngo <kareem.kwong@gmail.com>
2015-04-17 15:06:12 +0800

    Working on hotness method

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,7 +8,17 @@ class Product < ActiveRecord::Base
   scope :womens, lambda { where(for_men: false )}
 
   def self.trending
-    order(upvotes_count: :asc).limit(50)
+    all
+  end
+
+  def hotness
+    num_of_likes = self.liked_by_count
+    time_diff = (Time.now - self.created_at)
+    if time_diff > 1
+      score = num_of_likes * time_diff
+    end
+
+    score
   end
 
 
