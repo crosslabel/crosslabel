523feeb58e791610055be3d6749e8ef3611bc608
kwngo <kareem.kwong@gmail.com>
2015-04-26 08:51:21 -0400

    Updated home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,11 @@
 class HomeController < ApplicationController
   def index
-    @products = Product.includes(:categories).order(created_at: :desc).limit(16)
+    @products = Product.all.order(created_at: :desc).limit(16)
     render :layout => "transparent_header"
   end
 
   def explore
-    @products = Product.includes(:categories).order(created_at: :desc).page(params[:page]).per(20)
+    @products = Product.all.order(created_at: :desc).page(params[:page]).per(20)
     @categories = Category.all
     @retailers = Retailer.all.limit(10)
   end
