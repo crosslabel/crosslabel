784a10dd0db09778312e7aa15e4a5c6a29444278
kwngo <kareem.kwong@gmail.com>
2015-05-05 15:57:30 -0400

    Added devise secret key

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -10,7 +10,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-
+  config.secret_key = Rails.application.secrets.devise_secret_key
   config.scoped_views = true
   config.mailer_sender = 'Kareem at Crosslabel <kareem@crosslabel.com>'
 
