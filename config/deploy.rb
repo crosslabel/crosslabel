da9a45cdca32ed3f8f66aef4b143e995018975eb
kwngo <kareem.kwong@gmail.com>
2015-05-07 10:52:21 -0400

    Update config for Digital Ocean

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, 'git@github.com:crosslabel/crosslabel.git'
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, '/var/www/ruby_apps/crosslabel'
+set :deploy_to, '/home/deploy/crosslabel'
 
 # Default value for :scm is :git
 # set :scm, :git
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -6,7 +6,7 @@
 # server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
-set :rails_env, 'staging'
+set :rails_env, :staging
 server '162.243.217.211', user: 'deploy', roles: %w{web app}
 # role-based syntax
 # ==================
