d6520aff3953d6aa1d18d63299bb0ad87c47b5fb
kwngo <kareem.kwong@gmail.com>
2015-05-07 14:46:09 -0400

    Move segment write key to secrets.yml

--- config/initializers/analytics_ruby.rb
+++ config/initializers/analytics_ruby.rb
@@ -1,4 +1,4 @@
 Analytics = Segment::Analytics.new({
-    write_key: 'N2AR8Z7srhCpK3nS3KAnmkKsbezxEgLc',
+    write_key: Rails.application.secrets.segment_write_key,
     on_error: Proc.new { |status, msg| print msg }
 })
