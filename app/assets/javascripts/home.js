187b11078ab0747387d60fa45186194bc31d669d
kwngo <kareem.kwong@gmail.com>
2015-05-08 16:42:08 -0400

    Removed infinite scroll

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,26 +2,26 @@
 
 
 // Kaminari pagination
-$(document).ready(function() {
-  $('#next').click(function() {
-    $(".products-list").infinitescroll({
-      loading: {
-        finishedMsg: '<p>No more items :(</p>',
-        img: "",
-        msgText: "<span class='loader'></span>"
-      },
-      navSelector: "nav.pagination",
-      nextSelector: "nav.pagination a[rel=next]",
-      itemSelector: ".products-list",
-      });
-  });
-
-  $(window).unbind('.infscr');
-
-  $('#next').click(function() {
-    $(".products-list").infinitescroll('retrieve');
-    $("#next").fadeOut();
-  });
-  return false;
-
-});
+// $(document).ready(function() {
+//   $('#next').click(function() {
+//     $(".products-list").infinitescroll({
+//       loading: {
+//         finishedMsg: '<p>No more items :(</p>',
+//         img: "",
+//         msgText: "<span class='loader'></span>"
+//       },
+//       navSelector: "nav.pagination",
+//       nextSelector: "nav.pagination a[rel=next]",
+//       itemSelector: ".products-list",
+//       });
+//   });
+//
+//   $(window).unbind('.infscr');
+//
+//   $('#next').click(function() {
+//     $(".products-list").infinitescroll('retrieve');
+//     $("#next").fadeOut();
+//   });
+//   return false;
+//
+// });
