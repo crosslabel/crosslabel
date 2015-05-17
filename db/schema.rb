ee981f2ea3fe896647c0c6f74b5c5e9c20af296e
kwngo <kareem.kwong@gmail.com>
2015-05-17 19:52:41 -0400

    Added description to retailers

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150429210735) do
+ActiveRecord::Schema.define(version: 20150517234957) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "admins", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "role"
+  end
+
+  add_index "admins", ["email"], name: "index_admins_on_email", unique: true, using: :btree
+  add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true, using: :btree
 
   create_table "authentications", force: true do |t|
     t.string   "provider"
@@ -111,6 +130,7 @@ ActiveRecord::Schema.define(version: 20150429210735) do
     t.integer  "city_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "description"
   end
 
   add_index "retailers", ["city_id"], name: "index_retailers_on_city_id", using: :btree
