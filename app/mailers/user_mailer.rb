3abfda480f5c3ca9777c725145eb6385cd93f05f
kwngo <kareem.kwong@gmail.com>
2015-05-22 09:09:03 -0400

    Update mandrill template

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,15 +8,19 @@ class UserMailer < Devise::Mailer
   end
 
   def welcome_email(user)
-    template_name = "welcome-email"
+    template_name = "crosslabel-default"
     template_content = []
     message = {
       to: [{email: user.email}],
-      subject: "Welcome to Brandly!",
+      subject: "Thanks for joining CrossLabel!",
       merge_vars: [
         {rcpt: user.email,
         vars: [
-          {name: "WELCOME_EMAIL_HEADING", content: "Welcome #{user.email}"}
+          {name: "WELCOME_EMAIL_HEADING", content: "Hi #{user.username}"},
+          {name: "LIST:DESCRIPTION", content: "By signing up for CrossLabel, you sign up for updates from our site. We'll be adding an opt-out option shortly."}
+          {name: "LIST:COMPANY", content: "CrossLabel"}
+          {name: "HTML:LIST_ADDRESS_HTML", content: "Toronto, Ontario, Canada"}
+
           ]}
       ]
     }
