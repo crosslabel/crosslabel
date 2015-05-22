434655aea899489077f8f6930e121b748d8ad1cd
kwngo <kareem.kwong@gmail.com>
2015-05-21 22:27:29 -0400

    Add validations for user_id and uid

--- app/models/authentication.rb
+++ app/models/authentication.rb
@@ -1,5 +1,7 @@
 class Authentication < ActiveRecord::Base
   belongs_to :user
+  validates_presence_of :user_id, :uid, :provider
+  validates_uniqueness_of :uid, :scope => :provider
 
   def self.find_with_omniauth(auth)
       Authentication.find_by(uid: auth['uid'], provider: auth['provider'])
@@ -9,7 +11,7 @@ class Authentication < ActiveRecord::Base
       user ||= User.create_with_omniauth(auth)
       Authentication.create(user_id: user.id, uid: auth['uid'], provider: auth['provider'])
   end
-  
+
   # def self.find_with_omniauth(auth)
   #   where(:provider => auth.provider, :uid => auth.uid.to_s, :token => auth.credentials.token).first_or_initialize
   # end
